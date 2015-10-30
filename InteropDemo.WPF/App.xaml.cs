d765899b9fca408eb01c61ae90d70c30a31a09da
Pavel Karpenko <pwkpwk@hotmail.com>
2015-10-30 11:41:34 -0700

    Minor cleanup

--- InteropDemo.WPF/App.xaml.cs
+++ InteropDemo.WPF/App.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace InteropDemo.WPF
+﻿namespace InteropDemo.WPF
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
